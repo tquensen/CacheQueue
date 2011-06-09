b9acbd9f7ec350adbcedc294644b541de0e7628e
tquensen <t.quensen@yeebase.com>
2011-06-09 13:47:27 +0200

    improved example client file

--- exampleClient.php
+++ exampleClient.php
@@ -1,24 +1,45 @@
 #!/usr/bin/php
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__).'/lib');
 
-$configFile = dirname(__FILE__).'/config.php';
+function getCacheQueueClient() {
+    static $client = null;
+    
+    if ($client) {
+        return $client;
+    }
+    
+    
+    //define config file
+    $configFile = dirname(__FILE__).'/config.php';
 
-$config = array();
-require_once($configFile);
+    
+    $config = array();
+    require_once($configFile);
 
-$connectionClass = $config['classes']['connection'];
-$clientClass = $config['classes']['client'];
-$connectionFile = str_replace('\\', DIRECTORY_SEPARATOR, trim($connectionClass, '\\')).'.php';
-$clientFile = str_replace('\\', DIRECTORY_SEPARATOR, trim($clientClass, '\\')).'.php';
+    $connectionClass = $config['classes']['connection'];
+    $clientClass = $config['classes']['client'];
+    
+    //load required classes - uncomment if you don't use an autoloader 
+    /*
+    $connectionFile = str_replace('\\', DIRECTORY_SEPARATOR, trim($connectionClass, '\\')).'.php';
+    $clientFile = str_replace('\\', DIRECTORY_SEPARATOR, trim($clientClass, '\\')).'.php';
 
-require_once('CacheQueue/IConnection.php');
-require_once('CacheQueue/IClient.php');
-require_once($connectionFile);
-require_once($clientFile);
+    //add CacheQueue parent folder to include path
+    set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__).'/lib');
+    require_once('CacheQueue/IConnection.php');
+    require_once('CacheQueue/IClient.php');
+    require_once($connectionFile);
+    require_once($clientFile);
+    */
+    
 
-$connection = new $connectionClass($config['connection']);
-$client = new $clientClass($connection);
+    $connection = new $connectionClass($config['connection']);
+    $client = new $clientClass($connection);
+    
+    return $client;
+}
 
+
+$client = getCacheQueueClient();
 $result = $client->getOrQueue('example_data', 'store', 'random '.  rand(100, 999), time() + 10);
 echo 'Client: result '.($result === false ? 'false' : $result).'.'."\n";
